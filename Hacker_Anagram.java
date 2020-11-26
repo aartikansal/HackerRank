d4284f5d889ded93511f7beda4c712c7fd84b598
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-11-25 20:25:34 -0500

    Add files via upload

---
+++ Hacker_Anagram.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hacker_Anagram {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a=a.toLowerCase();
+        b=b.toLowerCase();
+        boolean f = false;
+        char[] c = a.toCharArray();
+        Arrays.sort(c);
+        char[] d = b.toCharArray();
+        Arrays.sort(d);
+        String A = new String (c);
+        String B = new String (d);
+        if (A.equals(B)) {
+            f=true;
+        }
+        return f;
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
